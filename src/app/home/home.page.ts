8afba3733dbd601fa7cf4cc149c7ac6961f8abd2
Yusuf Ş. Tarhan <kastinleaker@gmail.com>
2020-05-01 01:38:15 +0300

    Update home.page.ts

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -34,6 +34,182 @@ export class HomePage {
         four: "4: four",
         five: "5: five"
       },
+      {
+        title: "How many options we have?",
+        one: "1: one",
+        two: "2: two",
+        three: "3: three",
+        four: "4: four",
+        five: "5: five"
+      },
+      {
+        title: "How many options we have?",
+        one: "1: one",
+        two: "2: two",
+        three: "3: three",
+        four: "4: four",
+        five: "5: five"
+      },
+      {
+        title: "How many options we have?",
+        one: "1: one",
+        two: "2: two",
+        three: "3: three",
+        four: "4: four",
+        five: "5: five"
+      },
+      {
+        title: "How many options we have?",
+        one: "1: one",
+        two: "2: two",
+        three: "3: three",
+        four: "4: four",
+        five: "5: five"
+      },
+      {
+        title: "How many options we have?",
+        one: "1: one",
+        two: "2: two",
+        three: "3: three",
+        four: "4: four",
+        five: "5: five"
+      },
+      {
+        title: "How many options we have?",
+        one: "1: one",
+        two: "2: two",
+        three: "3: three",
+        four: "4: four",
+        five: "5: five"
+      },
+      {
+        title: "How many options we have?",
+        one: "1: one",
+        two: "2: two",
+        three: "3: three",
+        four: "4: four",
+        five: "5: five"
+      },
+      {
+        title: "How many options we have?",
+        one: "1: one",
+        two: "2: two",
+        three: "3: three",
+        four: "4: four",
+        five: "5: five"
+      },
+      {
+        title: "How many options we have?",
+        one: "1: one",
+        two: "2: two",
+        three: "3: three",
+        four: "4: four",
+        five: "5: five"
+      },
+      {
+        title: "How many options we have?",
+        one: "1: one",
+        two: "2: two",
+        three: "3: three",
+        four: "4: four",
+        five: "5: five"
+      },
+      {
+        title: "How many options we have?",
+        one: "1: one",
+        two: "2: two",
+        three: "3: three",
+        four: "4: four",
+        five: "5: five"
+      },
+      {
+        title: "How many options we have?",
+        one: "1: one",
+        two: "2: two",
+        three: "3: three",
+        four: "4: four",
+        five: "5: five"
+      },
+      {
+        title: "How many options we have?",
+        one: "1: one",
+        two: "2: two",
+        three: "3: three",
+        four: "4: four",
+        five: "5: five"
+      },
+      {
+        title: "How many options we have?",
+        one: "1: one",
+        two: "2: two",
+        three: "3: three",
+        four: "4: four",
+        five: "5: five"
+      },
+      {
+        title: "How many options we have?",
+        one: "1: one",
+        two: "2: two",
+        three: "3: three",
+        four: "4: four",
+        five: "5: five"
+      },
+      {
+        title: "How many options we have?",
+        one: "1: one",
+        two: "2: two",
+        three: "3: three",
+        four: "4: four",
+        five: "5: five"
+      },
+      {
+        title: "How many options we have?",
+        one: "1: one",
+        two: "2: two",
+        three: "3: three",
+        four: "4: four",
+        five: "5: five"
+      },
+      {
+        title: "How many options we have?",
+        one: "1: one",
+        two: "2: two",
+        three: "3: three",
+        four: "4: four",
+        five: "5: five"
+      },
+      {
+        title: "How many options we have?",
+        one: "1: one",
+        two: "2: two",
+        three: "3: three",
+        four: "4: four",
+        five: "5: five"
+      },
+      {
+        title: "How many options we have?",
+        one: "1: one",
+        two: "2: two",
+        three: "3: three",
+        four: "4: four",
+        five: "5: five"
+      },
+      {
+        title: "How many options we have?",
+        one: "1: one",
+        two: "2: two",
+        three: "3: three",
+        four: "4: four",
+        five: "5: five"
+      },
+      {
+        title: "How many options we have?",
+        one: "1: one",
+        two: "2: two",
+        three: "3: three",
+        four: "4: four",
+        five: "5: five"
+      },
     ]
   };
 
